6573ce557b510307784d90ad9cdac1e289c77f59
minseo <msp05070478@gmail.com>
2021-11-21 22:52:14 +0900

    [ADD]Splash
    Splash화면 수정 및 전반적인 ui수정

--- app/src/main/java/com/example/sns_super_android/sns/splash/IntroActivity.kt
+++ app/src/main/java/com/example/sns_super_android/sns/splash/IntroActivity.kt
@@ -3,14 +3,30 @@ package com.example.sns_super_android.sns.splash
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import com.example.sns_super_android.R
 import com.example.sns.login.ui.LoginActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class IntroActivity : AppCompatActivity() {
+    private var anim01: Animation? = null
+    private var anim02: Animation? = null
+    private var smallStick: View? = null
+    private var bigStick: View? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+        smallStick = findViewById<View>(R.id.last_stick_1)
+        anim02 = AnimationUtils.loadAnimation(this, R.anim.intro_anim_2)
+        smallStick!!.startAnimation(anim02)
+
+        bigStick = findViewById<View>(R.id.first_stick_1)
+        anim01 = AnimationUtils.loadAnimation(this, R.anim.intro_anim)
+        bigStick!!.startAnimation(anim01)
         val handler = Handler()
         handler.postDelayed({
             val intent = Intent(this, LoginActivity::class.java)
